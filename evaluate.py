f19fc2ad2eb0b8044aae93bfdd0ff22acc327e43
TAN <46641142+JingangTan@users.noreply.github.com>
2019-03-21 16:45:56 +0800

    Update evaluate.py

--- evaluate.py
+++ evaluate.py
@@ -27,7 +27,7 @@ RESTORE_FROM = './model/GTA2City_Resnet.pth'
 RESTORE_FROM_VGG = './model/GTA2City_Vgg.pth'
 SET = 'val'
 
-MODEL = 'DeeplabVGG'
+MODEL = 'DeeplabMulti'
 
 palette = [128, 64, 128, 244, 35, 232, 70, 70, 70, 102, 102, 156, 190, 153, 153, 153, 153, 153, 250, 170, 30,
            220, 220, 0, 107, 142, 35, 152, 251, 152, 70, 130, 180, 220, 20, 60, 255, 0, 0, 0, 0, 142, 0, 0, 70,
