84ccd50e526a06b8b2a12f76a753d4e860d0f9eb
TAN <46641142+JingangTan@users.noreply.github.com>
2019-03-21 16:33:46 +0800

    Add files via upload

---
+++ requires.sh
@@ -0,0 +1,5 @@
+pip install numpy==1.14.5
+pip install pillow==5.4.1
+pip3 install https://download.pytorch.org/whl/cu80/torch-0.4.1-cp35-cp35m-linux_x86_64.whl
+pip install torchvision==0.2.0
+pip install packaging==18.0
